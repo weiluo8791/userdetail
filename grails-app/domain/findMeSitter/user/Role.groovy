17ffc0c5f34df49137b6393478bdc85d6f8c0482
Wei <iqboss@gmail.com>
2017-04-29 01:07:12 -0400

    Change domain add more relationship

--- grails-app/domain/findMeSitter/user/Role.groovy
+++ grails-app/domain/findMeSitter/user/Role.groovy
@@ -7,6 +7,10 @@ import groovy.transform.ToString
 @ToString(includes='authority', includeNames=true, includePackage=false)
 class Role implements Serializable {
 
+	public static final String ROLE_ADMIN =  'ROLE_ADMIN'
+	public static final String ROLE_USER =  'ROLE_USER'
+	public static final String ROLE_ANONYMOUS =  'ROLE_ANONYMOUS'
+
 	private static final long serialVersionUID = 1
 
 	String authority
